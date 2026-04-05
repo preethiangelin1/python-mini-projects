f9de5fa75f0ee2475cdbd8bc7cb7272e68fa893e
Preethi Angelin <preethiangelin1@gmail.com>
2026-04-05 22:25:16 +0530

    add password strength checker and generator

---
+++ 16_password_strength_checker/main.py
@@ -0,0 +1,51 @@
+import string
+import random
+import getpass
+
+def check_password_strength(password):
+    issues = []
+    if len(password) < 8:
+        issues.append("Too short (minimum 8 characters)")
+    if not any(c.islower() for c in password):
+        issues.append("Missing lower case letter")
+    if not any(c.isupper() for c in password):
+        issues.append("Missing upper case letter")
+    if not any(c.isdigit() for c in password):
+        issues.append("Missing a digit")
+    if not any(c in string.punctuation  for c in password):
+        issues.append("Missing a special character")
+    return issues
+    
+
+def generate_strong_password(length=12):
+    password = [
+        random.choice(string.ascii_uppercase),  # 1 uppercase
+        random.choice(string.ascii_lowercase),  # 1 uppercase
+        random.choice(string.digits),           # 1 digit
+        random.choice(string.punctuation)       # 1 special char
+    ]
+
+    all_chars = string.ascii_letters + string.digits + string.punctuation
+
+    for _ in range(length - 4):
+        password.append(random.choice(all_chars))
+
+    random.shuffle(password)
+
+    result = "".join(password)
+    return result
+
+if __name__ == "__main__":
+    password = getpass.getpass("Enter a password: ")
+    issues = check_password_strength(password)
+
+    if not issues:
+        print("Strong password! you are good to go")
+    else:
+        print("You got weak password")
+        for issue in issues:
+            print(f"- {issue}")
+
+    suggestion = generate_strong_password()
+    print("\n suggesting you a strong password")
+    print(suggestion)      
